c63266798836444e91a6ffd76691f467538bf47f
Victory <blazehoodz@gmail.com>
2024-10-09 10:28:29 +0100

    customer service

---
+++ services/customer/src/main/java/com/blaze/ecommerce/customer/CustomerController.java
@@ -0,0 +1,37 @@
+package com.blaze.ecommerce.customer;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+@RequiredArgsConstructor
+public class CustomerController {
+
+    private final CustomerService service;
+
+    @PostMapping
+    public ResponseEntity<String> createCustomer(@RequestBody @Valid CustomerRequest request){
+        return ResponseEntity.ok(service.createCustomer(request));
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateCustomer(@RequestBody @Valid CustomerRequest request){
+        service.updateCustomer(request);
+        return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerResponse>> findAll(){
+        return ResponseEntity.ok(service.findAllCustomers());
+    }
+
+    @GetMapping("/exists/{customer-id}")
+    public ResponseEntity<Boolean> existsById(@PathVariable("customer-id") String customerId){
+        return ResponseEntity.ok(service.existsById(customerId));
+    }
+}
